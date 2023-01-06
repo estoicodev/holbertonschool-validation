85a9764c8ecfccf0c351df0e5522630a021bcac9
Mauricio2802 <vmcarrasco2810@gmail.com>
2023-01-06 00:28:35 -0500

    Add a disposable environment

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# Run a disposable Ubuntu 18.04 environment with Docker
+sudo docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
 
 # Install necessary packages
 apt-get update && apt-get install -y hugo make
 
 # Run the build command from the Makefile
 make build
+
+# Exit
+exit
