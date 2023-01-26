e009d9c8c3146d9506767fc06e9166b3580c8a1e
Mauricio2802 <vmcarrasco2810@gmail.com>
2023-01-25 21:41:03 -0500

    Update setup.sh and workflow

--- module4_task1/setup.sh
+++ module4_task1/setup.sh
@@ -6,12 +6,6 @@ apt-get update && apt-get install -y make
 # Install curl to get latest release of hugo
 apt-get install curl
 
-# Use curl to download latest release of hugo
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
-
-# Install hugo
-apt-get install ./hugo.deb
-
 # Install markdownlint
 sudo npm install -g markdownlint-cli
 
